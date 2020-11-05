f434a90fb2e6c9c83b3d590fd111936c73075373
KotaIkehara <ikehara.aurora95@gmail.com>
2020-11-05 10:30:22 +0900

    Finish carousel

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"unicode/utf8"
 
 	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/linebot"
@@ -78,10 +81,79 @@ func sendRestoInfo(bot *linebot.Client, e *linebot.Event) {
 	lat := strconv.FormatFloat(msg.Latitude, 'f', 2, 64)
 	lng := strconv.FormatFloat(msg.Longitude, 'f', 2, 64)
 
-	replyMsg := fmt.Sprintf("緯度：%s\n経度：%s", lat, lng)
+	replyMsg := getRestoInfo(lat, lng)
 
-	_, err := bot.ReplyMessage(e.ReplyToken, linebot.NewTextMessage(replyMsg)).Do()
+	res := linebot.NewTemplateMessage(
+		"レストラン一覧",
+		linebot.NewCarouselTemplate(replyMsg...).WithImageOptions("rectangle", "cover"),
+	)
+
+	_, err := bot.ReplyMessage(e.ReplyToken, res).Do()
 	if err != nil {
 		log.Print(err)
 	}
+}
+
+//レスポンスの構造体定義
+type response struct {
+	Results results `json:"results"`
+}
+type results struct {
+	Shop []shop `json:"shop"`
+}
+type shop struct {
+	Name    string `json:"name"`
+	Address string `json:"address"`
+	Photo   photo  `json:"photo"`
+	URLS    urls   `json:"urls"`
+}
+
+type photo struct {
+	Mobile mobile `json:"mobile"`
+}
+
+type mobile struct {
+	L string `json:"l"`
+}
+
+type urls struct {
+	PC string `json:"pc"`
+}
+
+func getRestoInfo(lat string, lng string) []*linebot.CarouselColumn {
+	url := fmt.Sprintf(
+		"https://webservice.recruit.co.jp/hotpepper/gourmet/v1/?format=json&key=%s&lat=%s&lng=%s",
+		os.Getenv("HOTPEPPER_API_KEY"), lat, lng)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data response
+	if err := json.Unmarshal(body, &data); err != nil {
+		log.Fatal(err)
+	}
+
+	var ccs []*linebot.CarouselColumn
+	for _, shop := range data.Results.Shop {
+		addr := shop.Address
+		if 60 < utf8.RuneCountInString(addr) {
+			addr = string([]rune(addr)[:60])
+		}
+
+		cc := linebot.NewCarouselColumn(
+			shop.Photo.Mobile.L,
+			shop.Name,
+			addr,
+			linebot.NewURIAction("ホットペッパーで開く", shop.URLS.PC),
+		).WithImageOptions("#FFFFFF")
+		ccs = append(ccs, cc)
+	}
+	return ccs
 }
